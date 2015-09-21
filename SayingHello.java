c297935b25803b3c1c57d434607cb74d1f594bc0
Ingo Hotischeck <i.hotischeck@iks-gmbh.com>
2015-09-21 11:16:19 +0200

    SayingHello.java

---
+++ SayingHello.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+public class SayingHello {
+	public static void main (String args[]) {
+	
+		System.out.print("What is your name? ");
+	
+		try {
+			InputStream inputStream = System.in;
+			BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+		
+			String name = reader.readLine();
+			
+			if (name.equals("Brian")) {
+				System.out.println("Hello, "+ name + ". Go to hell!");
+			}
+		
+			else if (name.equals("Ingo")) {
+				System.out.println("Welcome back, Commander "+ name);
+			}
+		
+			else if (name.equals("Hartwig")) {
+				System.out.println("Hello, " + name + " T\u00F6dter. I knew you would look into this program some day.");
+			}
+		
+			else {
+				System.out.println("Hello, "+ name + ", nice to meet you!");
+			}
+		}
+	
+		catch (IOException exception) {
+			System.out.println("Falscheingabe!");
+		}	
+	}
+}
